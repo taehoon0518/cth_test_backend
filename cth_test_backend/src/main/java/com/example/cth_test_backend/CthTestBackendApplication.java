f47d4a53698c46a8a5b355fb509af1842afd2157
taehoon0518 <cjsxogns0518@gmail.com>
2025-02-26 09:18:26 +0900

    [Init] 프로젝트 생성

---
+++ cth_test_backend/src/main/java/com/example/cth_test_backend/CthTestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cth_test_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CthTestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CthTestBackendApplication.class, args);
+    }
+
+}
---
+++ cth_test_backend/src/test/java/com/example/cth_test_backend/CthTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cth_test_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CthTestBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
