d40166c2ca7e20c7a056c0d7cea20df906f1e814
taehoon0518 <cjsxogns0518@gmail.com>
2025-02-26 11:04:00 +0900

    [Feat] 댓글 수 증가 기능 추가

--- cth_test_backend/src/main/java/com/example/cth_test_backend/board/BoardRepository.java
+++ cth_test_backend/src/main/java/com/example/cth_test_backend/board/BoardRepository.java
@@ -3,5 +3,8 @@ package com.example.cth_test_backend.board;
 import com.example.cth_test_backend.board.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.nio.CharBuffer;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    CharBuffer findByIdx(Long idx);
 }
